28465551e5fb2fb8a13f6ac64bb3b78ab6ead886
Javier <jab3213@utulsa.edu>
2020-09-25 01:04:54 +0200

    funciona, septiembre 25, 1:04am despues de instalar material

---
+++ src/app/admin/admin.component.ts
@@ -0,0 +1,74 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { PlanService } from 'src/app/auth/services/plan.service';
+import { PlanI } from 'src/app/shared/models/planes.interface';
+
+@Component({
+  selector: 'app-admin',
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.css'],
+})
+export class AdminComponent implements OnInit {
+  planes: PlanI[];
+  nuevoPlanForm = new FormGroup({
+    uid: new FormControl(''),
+    area: new FormControl(''),
+    tipo: new FormControl(''),
+    nombre: new FormControl(''),
+    subtitulo: new FormControl(''),
+    precio: new FormControl(''),
+    descripcion: new FormControl(''),
+  });
+
+  constructor(private router: Router, private planSvc: PlanService) {}
+
+  ngOnInit() {
+    this.planSvc.getPlanes().subscribe((data) => {
+      this.planes = data.map((e) => {
+        return {
+          id: e.payload.doc.id,
+          ...(e.payload.doc.data() as PlanI),
+        } as PlanI;
+      });
+    });
+  }
+
+  create(plan: PlanI) {
+    this.planSvc.createPlan(plan);
+  }
+  update(plan: PlanI) {
+    this.planSvc.updatePlan(plan);
+  }
+  async onNewPlan() {}
+
+  // async onNewPlan() {
+  //   const {
+  //     uid,
+  //     area,
+  //     tipo,
+  //     nombre,
+  //     subtitulo,
+  //     precio,
+  //     descripcion,
+  //   } = this.nuevoPlanForm.value;
+  //   {
+  //     try {
+  //       const plan = await this.planSvc.nuevoPlan(
+  //         uid,
+  //         area,
+  //         tipo,
+  //         nombre,
+  //         subtitulo,
+  //         precio,
+  //         descripcion
+  //       );
+  //       if (plan) {
+  //         window.alert('Se ha añadido el plan nuevo correctamente.');
+  //       }
+  //     } catch (error) {
+  //       window.alert(error);
+  //     }
+  //   }
+  // }
+}
